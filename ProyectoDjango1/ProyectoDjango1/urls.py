02333a68e11fdd6445d6457dd6bbd325712e6bea
Gustavo Diaz Saavedra <gadiazsaavedra@gmail.com>
2022-10-19 14:34:42 -0300

    con funcion fecah de hoy

--- ProyectoDjango1/ProyectoDjango1/urls.py
+++ ProyectoDjango1/ProyectoDjango1/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ProyectoDjango1.views import saludo
+from ProyectoDjango1.views import saludo, hoy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("saludando/", saludo),
+    path("hoy/", hoy),
 ]
--- ProyectoDjango1/ProyectoDjango1/views.py
+++ ProyectoDjango1/ProyectoDjango1/views.py
@@ -1,4 +1,5 @@
 """Creamos ProyectoCuello"""
+from datetime import datetime
 from django.http import HttpResponse
 
 
@@ -12,3 +13,15 @@ def saludo(request: bool) -> HttpResponse:
         HttpResponse: _description_
     """
     return HttpResponse("Hola mundo")
+
+
+def hoy(request: bool) -> HttpResponse:
+    """_summary_
+
+    Args:
+        request (bool): _description_
+
+    Returns:
+        HttpResponse: _description_
+    """
+    return HttpResponse(f"Hoy es : {datetime.now()}")
